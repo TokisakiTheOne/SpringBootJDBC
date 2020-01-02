98c134f4655457ae3066adfd6ef614ea8647f8ae
YanYuhang <yuhang.yan@gzfbdqn.com>
2020-01-02 11:53:26 +0800

    新增了thymeleaf

--- src/main/java/com/yyh/handler/DeptHandler.java
+++ src/main/java/com/yyh/handler/DeptHandler.java
@@ -3,7 +3,10 @@ package com.yyh.handler;
 import com.yyh.dao.DeptDaoImpl;
 import com.yyh.po.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,18 +15,23 @@ import java.util.List;
  * @author YanYuHang
  * @create 2020-01-02-9:43
  */
-@RestController
+@Controller
 @RequestMapping("/dept")
 public class DeptHandler {
     @Autowired
     private DeptDaoImpl dd;
 
-    @RequestMapping("/")
+    @RequestMapping("/selectAll")
+    @ResponseBody
     public List<Dept> selectAll(){
         return dd.selectAll();
     }
 
-
+    @RequestMapping("/")
+    public String selectAllDept(Model model){
+        model.addAttribute("deptList",dd.selectAll());
+        return "dept";
+    }
 
 
 
--- src/main/java/com/yyh/handler/EmpHandler.java
+++ src/main/java/com/yyh/handler/EmpHandler.java
@@ -5,7 +5,10 @@ import com.yyh.dao.EmpDaoImpl;
 import com.yyh.po.Dept;
 import com.yyh.po.Emp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,15 +17,21 @@ import java.util.List;
  * @author YanYuHang
  * @create 2020-01-02-10:55
  */
-@RestController
+@Controller
 @RequestMapping("/emp")
 public class EmpHandler {
     @Autowired
     private EmpDaoImpl ed;
-
-    @RequestMapping("/")
+    @ResponseBody
+    @RequestMapping("/selectAll")
     public List<Emp> selectAll() {
         return ed.selectAll();
+    }
+
+    @RequestMapping("/")
+    public String selectAllEmp(Model model) {
+        model.addAttribute("empList",ed.selectAll());
+        return "emp";
     }
 
 }
